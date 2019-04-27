cc84676aaa5f1dc9c1a5daee2b31ff0b056361d1
Jack <jack@Jacks-MacBook-Pro.local>
2019-04-27 00:38:54 -0700

    first step of to do app

---
+++ TodoAppAdv/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TodoAppAdv
+//
+//  Created by Jack on 4/25/19.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    let itemArray = ["milk", "whatecer"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
